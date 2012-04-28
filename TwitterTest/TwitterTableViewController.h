d75300b0dc80da70ad06398575ec4c2e1fa111cc
mustafa <mustafaf@gmail.com>
2012-04-28 00:15:37 -0700

    First working version

---
+++ TwitterTest/TwitterTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TwitterTableViewController.h
+//  TwitterTest
+//
+//  Created by Mustafa Furniturewala on 4/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwitterTableViewController : UITableViewController
+
+@end
